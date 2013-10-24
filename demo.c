a7d9d12dbfaebc0f9871aa725f94609b9240d448
Heramerom <heramerom@163.com>
2013-10-24 11:11:55 +0800

    demo.c
    demo.c

---
+++ demo.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: demo.c
+	> Author: heramerom
+	> Mail: heramerom@163.com
+	> Created Time: 四 10/24 11:10:44 2013
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
+
